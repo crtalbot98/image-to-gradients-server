6ac6cc0898b8b13145faa8a4d54a78b25c7663a6
Chandler Talbot <crtalbot98@gmail.com>
2020-09-26 16:48:24 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const http = require('http');
+const https = require('https');
+const url = require('url');
+
+(function(){
+    const port = 8080;
+
+    let server = http.createServer(function (req, res) {
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        });
+        const parsedUrl = url.parse(req.url, true);
+
+        if(parsedUrl.pathname === '/images') requestData(parsedUrl.query.page).then((data) => {
+            res.write(data);
+            res.end()
+        });
+    });
+
+    server.listen(port)
+})();
+
+const requestData = (page) => {
+    return new Promise((resolve, reject) => {
+        https.get(`https://api.unsplash.com/photos/?page=${page}&per_page=${12}&client_id=${process.env.IMAGE_ACCESS}`, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk
+            });
+
+            res.on('end', () => {
+                resolve(JSON.stringify(data))
+            });
+
+        }).on('error', (err) => {
+            reject(JSON.stringify({error: `please try again, ${err}`}))
+        })
+    });
+
+};
